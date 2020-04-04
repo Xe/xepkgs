ecfa6a3009f84d1b8c7b30265f4cc26a09eb7633
Christine Dodrill <me@christine.website>
2020-04-04 11:56:02 -0400

    lol oops

--- pkgs/johaus/default.nix
+++ pkgs/johaus/default.nix
@@ -13,7 +13,7 @@ let
     pname = "johaus";
     version = version;
     src = repo;
-    modSha256 = "sha256:1s3yj3b5zs9cqss2p2qdsk5yxmwna53nnk62zf78j6whan2kqags";
+    modSha256 = "1s3yj3b5zs9cqss2p2qdsk5yxmwna53nnk62zf78j6whan2kqags";
 
     subPackages = [ "." ];
   };

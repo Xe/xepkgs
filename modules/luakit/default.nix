9254284e233ffab27f626a523de0cc56d22a8c82
Christine Dodrill <me@christine.website>
2020-04-20 16:35:09 -0400

    luakit module

---
+++ modules/luakit/default.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+
+let
+  nur = import (builtins.fetchTarball
+    "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+in {
+  home = {
+    packages = [ nur.repos.xe.luakit ];
+    file = {
+      ".local/share/luakit/newtab.html".source = ./start.html;
+      ".config/luakit/userconf.lua".text = ''
+        require_web_module("referer_control_wm")
+        require_web_module("tab_favicons")
+        settings.window.home_page = "luakit://newtab/"
+      '';
+    };
+  };
+}
